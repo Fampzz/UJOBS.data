c7111149d769312af61407bf89786e8704962aa6
luisfelipegomesr <luisfelipegomesr@gmail.com>
2023-05-01 23:55:10 -0300

    Update login-script.js

--- login-script.js
+++ login-script.js
@@ -4,7 +4,7 @@ function fazerLogin() {
     const email = document.getElementById("email-login").value;
     const senha = document.getElementById("senha-login").value;
     if (email === "admin@test.com" && senha === "adm123") {
-        
+        window.location.href="https://ujobs.vercel.app/freelancers.html?"
     } else {
         alert("E-mail ou senha incorretos.");
     }
