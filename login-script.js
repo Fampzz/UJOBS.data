6ff4b6558bee6adbd1b070e06e039e827d168875
luisfelipegomesr <luisfelipegomesr@gmail.com>
2023-05-02 00:56:19 -0300

    Update login-script.js

--- login-script.js
+++ login-script.js
@@ -2,11 +2,11 @@ function fazerLogin() {
     const email = document.getElementById("email-login").value;
     const senha = document.getElementById("senha-login").value;
     if (email === "admin@test.com" && senha === "adm123") {
-        window.open("http://127.0.0.1:5500/freelancers.html")
+        window.open("http://127.0.0.1:5500/Principal.html")
     } else {
         alert("E-mail ou senha incorretos.");
     }
   }
   function redirecionarParaPagina() {
-    window.open("http://127.0.0.1:5500/freelancers.html")
+    window.open("http://127.0.0.1:5500/Principal.html")
   }
